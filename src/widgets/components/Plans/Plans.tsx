bc74c937a237097c41d51d59c003fb34c083271b
EaleksS <ealgenv@gmail.com>
2023-05-29 18:34:33 +0900

    .

--- src/widgets/components/Plans/Plans.tsx
+++ src/widgets/components/Plans/Plans.tsx
@@ -14,19 +14,28 @@ export const Plans: FC = (): JSX.Element => {
       </Text>
       <div className={styles.items}>
         <div className={styles.item}>
-          <img src="/i3.webp" alt="svg" />
+          <img
+            src="https://i.1.creatium.io/6b/2e/b7/2bebf1b2728723d93857de2315d0640d78/db0e6b2e08315a103517925df2a380bf.png"
+            alt="svg"
+          />
           <Text type="h4" fw="600">
             Типовой план этажа
           </Text>
         </div>
         <div className={styles.item}>
-          <img src="/i3.webp" alt="svg" />
+          <img
+            src="https://i.1.creatium.io/52/da/a0/5f2413d4a97d2727c8c47d1c725292fd1d/2e2b763859c76c5976b05d2cd0422ee4.png"
+            alt="svg"
+          />
           <Text type="h4" fw="600">
             Планировка 1-комн 37 м2
           </Text>
         </div>
         <div className={styles.item}>
-          <img src="/i3.webp" alt="svg" />
+          <img
+            src="https://i.1.creatium.io/70/35/fe/bfa0e4e619da97ada5046b2b93bb0a9568/4a39d012a1efe822373063f5050899b4.png"
+            alt="svg"
+          />
           <Text type="h4" fw="600">
             Планировка 2-комн 60 м2
           </Text>
