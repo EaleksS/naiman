5d13e11c2f1e7de51ee421c385eb5547e670415b
EaleksS <ealgenv@gmail.com>
2023-05-29 17:49:21 +0900

    .

--- src/widgets/components/Gallery/Gallery.tsx
+++ src/widgets/components/Gallery/Gallery.tsx
@@ -15,46 +15,14 @@ export const Gallery: FC = (): JSX.Element => {
 
       <div className={styles.items}>
         <img className={styles.item} src="/preview.jpg" alt="jpg" />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/67/05/77/dad60af6d8cba7308bfba7bde9df1802b4/438x330q8/c66f2179acb7628f3b5f2c92170e4acb.jpg"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/3c/ea/85/b48082988b041855cc82904c1a52075e32/587x330q8/whatsapp_image_2023_05_22_at_19_14_33_1.jpg"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/a5/91/6a/057308762c54363f22985da67e8c1d4ce7/441x330q8/234324324324.png"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/e9/23/4b/54d7191a246ab8fff1cd8d4358ed7d821c/350x350q8/whatsapp_image_2023_05_22_at_19_14_36_2.jpg"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/ec/73/80/a2cc096ac1412cfbd2f9de0e13adc4b599/587x330q8/whatsapp_image_2023_05_22_at_19_14_33_2.jpg"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/65/20/7c/7782b11946a00f9e7918ff5b503eac9505/350x350q8/whatsapp_image_2023_05_22_at_19_14_37.jpg"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/5d/65/7a/693e0d7c8c2f1fa6af36be6d84d139d64f/442x330q8/4482c7f68e40f5c1b0e503309607130d.jpg"
-          alt="gif"
-        />
-        <img
-          className={styles.item}
-          src="https://i.1.creatium.io/60/23/0d/0207ad8c841e1c10e2d8e64e1f94e064f8/350x350q8/whatsapp_image_2023_05_22_at_19_14_35_1.jpg"
-          alt="gif"
-        />
+        <img className={styles.item} src="/img/1.jpg" alt="gif" />
+        <img className={styles.item} src="/img/2.jpg" alt="gif" />
+        <img className={styles.item} src="/img/3.png" alt="gif" />
+        <img className={styles.item} src="/img/4.jpg" alt="gif" />
+        <img className={styles.item} src="/img/5.jpg" alt="gif" />
+        <img className={styles.item} src="/img/6.jpg" alt="gif" />
+        <img className={styles.item} src="/img/7.jpg" alt="gif" />
+        <img className={styles.item} src="/img/8.jpg" alt="gif" />
       </div>
 
       <div className={styles.btn}>
@@ -65,11 +33,7 @@ export const Gallery: FC = (): JSX.Element => {
       </div>
 
       <div className={styles.img}>
-        <img
-          className={styles.img_preview}
-          src="https://i.1.creatium.io/ec/73/80/a2cc096ac1412cfbd2f9de0e13adc4b599/1600x900q8/whatsapp_image_2023_05_22_at_19_14_33_2.jpg"
-          alt="preview"
-        />
+        <img className={styles.img_preview} src="/img/9.jpg" alt="preview" />
         <div className={styles.bg}></div>
 
         <Text type="h2" color="#fff">
--- src/widgets/components/Location/Location.tsx
+++ src/widgets/components/Location/Location.tsx
@@ -8,62 +8,44 @@ export const Location: FC = (): JSX.Element => {
   const { setIsActive } = useStore();
 
   return (
-    <div className={styles.location}  id="location">
+    <div className={styles.location} id="location">
       <Text type="h2" mt="60px" fw="500">
         ЛОКАЦИЯ NAIMAN
       </Text>
 
       <div className={styles.items}>
         <div className={styles.item}>
-          <img
-            src="https://i.1.creatium.io/da/8a/87/a493d909be291bfdfd0efbfd241fccda31/2211212.jpg"
-            alt="svg"
-          />
+          <img src="/img/10.jpg" alt="svg" />
           <Text type="h4" fw="600">
             Пляж 5 мин
           </Text>
         </div>
         <div className={styles.item}>
-          <img
-            src="https://i.1.creatium.io/cd/df/14/18e4b97bef5eaba37b7dbf15cbf1a9223e/d60b51fd826b492145bf3e2ee77ddd50.jpg"
-            alt="svg"
-          />
+          <img src="/img/11.jpg" alt="svg" />
           <Text type="h4" fw="600">
             Ж/Д вокзал 10 мин
           </Text>
         </div>
         <div className={styles.item}>
-          <img
-            src="https://i.1.creatium.io/dc/34/62/8ad2f1b4bf186169d1a4a6e294012cab3a/mg_6418.jpg"
-            alt="svg"
-          />
+          <img src="/img/12.jpg" alt="svg" />
           <Text type="h4" fw="600">
             Центр Сочи 10 мин
           </Text>
         </div>
         <div className={styles.item}>
-          <img
-            src="https://i.1.creatium.io/75/49/e2/2387d82b64b9309126f16f439e4ef1824b/aeroport_sochi_taximus_arenda_minivena_s_voditelem_v_moskve_.jpg"
-            alt="svg"
-          />
+          <img src="/img/13.jpg" alt="svg" />
           <Text type="h4" fw="600">
             Аэропорт 19 мин
           </Text>
         </div>
         <div className={styles.item}>
-          <img
-            src="https://i.1.creatium.io/ec/2f/ee/99ff5601dcf26524e009ab4f48be63fee4/1638587632_70_krot_info_p_sochi_adler_olimpiiskii_park_krasi.jpg"
-            alt="svg"
-          />
+          <img src="/img/14.jpg" alt="svg" />
           <Text type="h4" fw="600">
             Олимпийский парк 30 мин
           </Text>
         </div>
         <div className={styles.item}>
-          <img
-            src="https://i.1.creatium.io/d5/df/3b/0b6513311157239f77b30873fb3586161c/depositphotos_33093235_xl_2015.jpg"
-            alt="svg"
-          />
+          <img src="/img/15.jpg" alt="svg" />
           <Text type="h4" fw="600">
             Красная Поляна 50 мин
           </Text>
